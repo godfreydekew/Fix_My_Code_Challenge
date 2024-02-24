8b5a46a3507d2ec621986409fc5b2046df769280
godfreydekew <dekewgodfrey@gmail.com>
2024-02-24 00:30:49 -0800

    Task 4

---
+++ 0x00-challenge/4-delete_dnodeint/2-sort.rb
@@ -0,0 +1,35 @@
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            x = result[i]
+            result.insert(i - 1, i_arg)
+            result.insert(i, x)
+            is_inserted = true
+            i += 1
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+
+end
+
+puts result
